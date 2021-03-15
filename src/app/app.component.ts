2aff9d22781dd12534caf244775caf656476f1c0
Gumpnart <gumpnart@gmail.com>
2021-03-15 23:47:45 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+import * as CustomEditor from '../vendor/ckeditor5/build/ckeditor';
+import { CKEditor5 } from '@ckeditor/ckeditor5-angular';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'text-editor';
+  editor = CustomEditor;
+
+  onEditorReady(editor: CKEditor5.Editor) {
+    editor.ui
+      .getEditableElement()
+      .parentElement.insertBefore(
+        editor.ui.view.toolbar.element,
+        editor.ui.getEditableElement()
+      );
+  }
 }
